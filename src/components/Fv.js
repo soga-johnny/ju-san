2f6b351c33912a7c4675720ed0f932ce799dda66
soga-johnny <soga.johnny@gmail.com>
2022-12-08 23:01:14 +0900

    three

--- src/components/Fv.js
+++ src/components/Fv.js
@@ -1,81 +1,24 @@
-// import Image from 'next/image'
-// import styles from '../components/Fv.module.scss'
-// import { useState, useRef } from "react";
-// import { Canvas, useFrame } from '@react-three/fiber';
+import React from "react";
+import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
-// import { OrbitControls } from '@react-three/drei';
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-
+// import { OrbitControls } from '@react-three/drei'
+import { Model } from './Model'
+import styles from '../components/Fv.module.scss'
 
 export function Fv() {
-    const gltf = useLoader(GLTFLoader, "../public/Three_13.glb")
+    // const gltf = useLoader(GLTFLoader, "../public/Three_13.glb")
+    // const gltf = new GLTFLoader("../public/Three_13.glb")
     return (
-     <canvas>
-         <ambientLight />
-      <directionalLight />
+        <div className={styles.fv} style={{ width: "860px", height: "70vh" }}>
+     <Canvas flat linear>
+         <ambientLight intensity={3}　/>
+         <directionalLight position={[4,-4,20]} intensity={40} />
       <Suspense fallback={null}>
-        <primitive object={gltf.scene} />
+        <Model />
       </Suspense>
-     </canvas>
+      {/* <OrbitControls /> */}
+     </Canvas>
+     </div>
     )
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const Box = () => {
-//     const ref = useRef(null);
-//     const [isHovered, setIsHovered] = useState(false);
-  
-//     useFrame(() => {
-//       ref.current.rotation.x += 0.02;
-//       ref.current.rotation.y += 0.02;
-//     }, []);
-  
-//     return (
-//       <mesh
-//         ref={ref}
-//         onPointerOver={() => setIsHovered(true)}
-//         onPointerOut={() => setIsHovered(false)}
-//       >
-//         <boxBufferGeometry args={isHovered ? [3.5, 3.5, 3.5] : [3, 3, 3]} />
-//         <meshLambertMaterial color={isHovered ? 0x276D7C : 0x8D7147} />
-//       </mesh>
-//     );
-//   };
-  
-//   export function Fv() {
-//     return (
-//         <div className={styles.fv}>
-//       <Canvas dpr={2}>
-//         <color attach="background" args={[0xF3F0EB]} />
-//         <ambientLight intensity={0.9} />
-//         <directionalLight intensity={0.5} position={[-10, 10, 10]} />
-//         <Box />
-//         <OrbitControls />
-//       </Canvas>
-
-      
-
-//       </div>
-//     );
-//   }
-  
-  
